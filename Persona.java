687e01408874fadeb01d464939cb235b63f18422
facu-et <156059654+facu-et@users.noreply.github.com>
2025-06-12 15:53:16 -0300

    Create Persona.java
    Clase Persona, que no tiene interfaz

---
+++ Persona.java
@@ -0,0 +1,61 @@
+package modelo;
+
+
+
+public class Persona{ //sin interfaz xq solo tiene atributos
+
+	//Atributos de una Persona estudiante de UADE
+	private String nombre;
+	private int dni;
+	private float promedio;
+	private int legajo;
+	
+	
+	//getter del promedio
+	public float getPromedio() {
+		return promedio;
+	}
+	
+	//tostring
+	@Override
+	public String toString() {
+		return "Persona [nombre=" + nombre + ", dni=" + dni + ", promedio=" + promedio + ", legajo=" + legajo + "]";
+	}
+	
+	//constructor vacio
+	public Persona() {
+		super();
+	}
+	
+	//constructor "lleno"
+	public Persona(String nombre, int dni, float promedio, int legajo) {
+		super();
+		this.nombre = nombre;
+		this.dni = dni;
+		this.promedio = promedio;
+		this.legajo = legajo;
+	}
+	
+	//setter del promedio
+	public void setPromedio(float promedio) {
+		this.promedio = promedio;
+	}
+	
+	//getter del nombre
+	public String getNombre() {
+		return nombre;
+	}
+	
+	//getter del dni
+	public int getDni() {
+		return dni;
+	}
+	
+	//getter del legajo universitario
+	public int getLegajo() {
+		return legajo;
+	}
+	
+	
+	
+}
